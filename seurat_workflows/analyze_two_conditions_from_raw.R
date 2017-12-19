8771bc66a8269ed387b68b7044f825ef575e0061
Pascal Timshel at Yggdrasil <pascal.timshel@gmail.com>
2017-12-19 01:19:02 +0100

    Fixed several bugs for analyze_two_con..raw.R
    The script was NOT functional in the previous commit.
    Added new workflow.
    Now tested and works.

--- seurat_workflows/analyze_two_conditions_from_raw.R
+++ seurat_workflows/analyze_two_conditions_from_raw.R
@@ -1,19 +1,60 @@
+# Title: Script to analyze and align two conditations directly from 'raw' 10x data.
+# Author: Pascal N. Timshel, Pers Lab
+# Date: December 2017
 
-# time Rscript align_datasets.R |& tee align_datasets.out.txt
-  # Bash version > 4
 
-######################### DENPENDENCIES ###############################
 
-# tibble (part of tidyverse)
-# plyr (plyr::join_all)
 
 ######################################################################
+############################## USAGE #################################
+######################################################################
+
+# time Rscript analyze_two_conditions_from_raw.R --analysis_conditions lira_vs_pf |& tee analyze_two_conditions_from_raw.out.txt
+# time Rscript analyze_two_conditions_from_raw.R --analysis_conditions lira_vs_pf --output_prefix testX --test_run
+
+######################################################################
+########################### OptParse ################################
+######################################################################
+library(optparse)
+
+# specify our desired options in a list
+# by default OptionParser will add an help option equivalent to 
+# make_option(c("-h", "--help"), action="store_true", default=FALSE, 
+#               help="Show this help message and exit")
+
+option_list <- list( 
+  make_option("--analysis_conditions", type="character",
+              help = "Choose from 'lira_vs_pf', 'hfd_vs_chow', 'hfd_vs_pf'"),
+  make_option("--output_prefix", type="character", default=NULL,
+              help = "Prefix for output files"),
+  make_option("--test_run", action="store_true", default=FALSE, 
+              help="Run in 'test mode': load only 500 cells and a limited number of highly expressed genes."),
+  make_option("--n_cores", type="integer", default=10,
+              help = "Number of cores to use for parallelization [default %default]")
+  
+)
+
+# get command line options, if help option encountered print help and exit,
+# otherwise if options not found on command line then set defaults, 
+opt <- parse_args(OptionParser(option_list=option_list))
 
 
-library(Seurat)
-library(tidyverse)
+analysis_conditions <- opt$analysis_conditions # get argument
 
-library(parallel)
+
+if (is.null(opt$output_prefix)) { # if no argument given, we set the output prefix to analysis_conditions
+  output_prefix <- analysis_conditions
+  print(sprintf("No output_prefix given. Using output_prefix = %s", output_prefix))
+} else {
+  output_prefix <- opt$output_prefix
+  print(sprintf("Got output_prefix %s", output_prefix))
+}
+
+
+FLAG_TEST_RUN <- opt$test_run # set to TRUE to load only 500 cells and a limited number of highly expressed genes
+# ^ uses Seurat.CreateObj.test_small_dataset()
+
+N_CORES <- opt$n_cores
 
 
 ######################################################################
@@ -21,36 +62,46 @@ library(parallel)
 ######################################################################
 
 
-# analysis_prefix will be used as prefix for output files.
-analysis_prefix <- "lira_vs_pf" 
-# analysis_prefix <- "hfd_vs_chow"
-# analysis_prefix <- "hfd_vs_pf"
+# IMPORTANT: condition1 and condition2 names *MUST* match the folder names in /data/sc-10x/data-runs/170612-perslab-arc_lira/
+if (analysis_conditions == "lira_vs_pf") {
+  condition1 <-"hfd_ad_lib_lira"
+  condition2 <-"hfd_pair_fed"
+} else if (analysis_conditions == "hfd_vs_chow") {
+  condition1 <-"hfd_ad_lib"
+  condition2 <-"chow"
+} else if (analysis_conditions == "hfd_vs_pf") {
+  condition1 <-"hfd_ad_lib"
+  condition2 <-"hfd_pair_fed"
+} else {
+  stop("Wrong argument for analysis_conditions")
+}
 
-condition1 <-"hfd_ad_lib_lira"
-condition2 <-"hfd_pair_fed"
-
-# condition1 <-"hfd_ad_lib"
-# condition2 <-"chow"
-
-# condition1 <-"hfd_ad_lib"
-# condition2 <-"hfd_pair_fed"
-
+print(sprintf("========== RUNNING analysis_conditions = %s ==========", analysis_conditions))
 
 
 res.primary <- 0.8 # the primary resolution - will be used for cluster marker identification.
 res2calculate <- c(0.4, 0.6, 0.8, 1.2, 1.6, 2, 4, 10) # resolutions to calculate for FindClusters
-  # OBS: res.primary must be included in res2calculate
-  # "name" in metadata is set via paste0("res.", r), so res=2.0 will be "res.2" and NOT "res.2.0"
-  # sprintf("res.%s", 2.0) --> "res.2"
-  # paste0("res.", 2.0) --> "res.2"
+# OBS: res.primary must be included in res2calculate
+# "name" in metadata is set via paste0("res.", r), so res=2.0 will be "res.2" and NOT "res.2.0"
+# sprintf("res.%s", 2.0) --> "res.2"
+# paste0("res.", 2.0) --> "res.2"
 
-
-FLAG_TEST_RUN <- FALSE # set to TRUE to load only 500 cells and a limited number of highly expressed genes
-  # ^ uses Seurat.CreateObj.test_small_dataset()
 
 N_DIMS <- 20 # dims.use for CCA (and tSNE)
 N_MIN_CELLS_PER_TREATMENT_FOR_DE_ANALYSIS <- 10 # used for condition_de_genes(seurat_obj)
-N_CORES <- 10
+
+
+######################################################################
+######################### DENPENDENCIES ##############################
+######################################################################
+
+library(Seurat)
+library(tidyverse)
+
+library(parallel)
+
+# tibble (part of tidyverse)
+# plyr (plyr::join_all)
 
 
 ######################################################################
@@ -63,18 +114,18 @@ Seurat.CreateObj.test_small_dataset <- function(list_name, list_of_matrix_data) 
   ### METHOD 1: you know the number of genes and cells (but might be 'low quality' cells/genes)
   #N_GENES <- 2000; N_CELLS <- 500 # make sure to include enough genes for HVG/PCA/CCA/tSNE
   #data <- as.matrix(list_of_matrix_data[[list_name]])[1:N_GENES, 1:N_CELLS] # ROWS=GENES; COLUMNS=CELLS
-    # ^ this way may not be the best way of subsetting if you the genes in the datasets are not in the same order.
-    # ^ then you will end up with data sets with few overlapping genes, and the seurat test might not run optimally.
+  # ^ this way may not be the best way of subsetting if you the genes in the datasets are not in the same order.
+  # ^ then you will end up with data sets with few overlapping genes, and the seurat test might not run optimally.
   #seurat_obj <- CreateSeuratObject(data, min.cells=0, min.genes=0, project=list_name) 
-    # no filtering of cells or genes.
-    # project specifies the 'orig.ident' column in @meta.data
+  # no filtering of cells or genes.
+  # project specifies the 'orig.ident' column in @meta.data
   
   ### METHOD 2: you QC genes/cells and control the number of cells (SubsetData)
   seurat_obj <- CreateSeuratObject(raw.data=list_of_matrix_data[[list_name]], min.cells=300, min.genes=1500, project=list_name)
   # seurat_obj <- SubsetData(seurat_obj, cells.use=WhichCells())
   seurat_obj <- SubsetData(seurat_obj, max.cells.per.ident=500, random.seed=1) # alternative: SetAllIdent(seurat_obj, id="orig.ident")
-    # ^ SubsetData calls WhichCells.
-    # ^ Seurat sets 'project name' (orig.ident) as default identity
+  # ^ SubsetData calls WhichCells.
+  # ^ Seurat sets 'project name' (orig.ident) as default identity
   # RESULT:
   # 6581 genes across 500 samples.
   # 8410 genes across 500 samples.
@@ -100,7 +151,7 @@ Seurat.Filter <- function(seurat_obj) {
   seurat_obj <- FilterCells(object = seurat_obj, subset.names = c("nGene", "percent.mito"), 
                             low.thresholds = c(200, -Inf), high.thresholds = c(4000,.3))
 }
-  
+
 #Normalize,scale,find var genes
 Seurat.NormAndScale <- function(seurat_obj) {
   seurat_obj<-NormalizeData(seurat_obj, normalization.method = "LogNormalize", scale.factor = 1e4)
@@ -144,7 +195,14 @@ condition_de_genes <- function(seurat_obj) {
     rename( UQ(rlang::sym("pct1")) := UQ(rlang::sym(sprintf("pct.1.%s",statistical_tests[1]))) ) %>% # renaming one pct.1 columns
     rename( UQ(rlang::sym("pct2")) := UQ(rlang::sym(sprintf("pct.2.%s",statistical_tests[1]))) ) %>% # renaming one pct.2 columns
     select(-starts_with("pct."), -starts_with("avg_logFC")) %>% # remove redundant columns
-    arrange( UQ(rlang::sym(sprintf("p_val.%s", tail(statistical_tests, n=1)))) ) # sort by p_val for the last run of statistical_tests
+    rename( avg_logFC = logFC ) %>%  # renaming BACK AGAIN
+    rename( UQ(rlang::sym("pct.1")) := pct1 ) %>%  # renaming BACK AGAIN, not sure if UQ(rlang::sym()) is needed
+    rename( UQ(rlang::sym("pct.2")) := pct2 ) %>%  # renaming BACK AGAIN, not sure if UQ(rlang::sym()) is needed
+    mutate(
+      p_val = UQ(rlang::sym(sprintf("p_val.%s", tail(statistical_tests, n=1)))),
+      p_val_adj = UQ(rlang::sym(sprintf("p_val_adj.%s", tail(statistical_tests, n=1))))
+    ) %>%
+  arrange( UQ(rlang::sym(sprintf("p_val.%s", tail(statistical_tests, n=1)))) ) # sort by p_val for the last run of statistical_tests
   
   ### SNITPPIT df.de_analysis
   # gene p_val.wilcox      logFC  pct1  pct2 p_val_adj.wilcox p_val.negbinom p_val_adj.negbinom
@@ -168,8 +226,8 @@ matrix.dat2 <- Read10X(paste0("/raid5/data/sc-10x/data-runs/170612-perslab-arc_l
 
 list_of_matrix_data <-list(matrix.dat1, matrix.dat2) # *OBS*: the ORDER is important.
 names(list_of_matrix_data) <- c(condition1, condition2) # IMPORTANT: *NAMED* LIST. Seurat.CreateObj() needs a named object
-  # names(list_of_matrix_data) ---> "hfd_ad_lib_lira" "hfd_pair_fed" 
-  # ^ the named list was not preserved in list_of_seurat_objs.
+# names(list_of_matrix_data) ---> "hfd_ad_lib_lira" "hfd_pair_fed" 
+# ^ the named list was not preserved in list_of_seurat_objs.
 
 ##############################################################################
 ############################# PROCESS DATA ###################################
@@ -227,9 +285,9 @@ hvg.union <- union(x = hvg.condition1, y = hvg.condition2)
 
 #Run Canonical Correlation Analysis
 seurat_merge <- RunCCA(object = list_of_seurat_objs[[1]], object2 = list_of_seurat_objs[[2]], genes.use = hvg.union, add.cell.id1=condition1, add.cell.id2=condition2) 
-  # Without add.cell.id1/2  --> ERROR: Duplicate cell names, please provide 'add.cell.id1' and/or 'add.cell.id2' for unique names
-  # With add.cell.id: "A bug about the alignment for two-datasets" (Oct 20th): https://github.com/satijalab/seurat/issues/184
-  # --> need to be on dev branch to fix this error.
+# Without add.cell.id1/2  --> ERROR: Duplicate cell names, please provide 'add.cell.id1' and/or 'add.cell.id2' for unique names
+# With add.cell.id: "A bug about the alignment for two-datasets" (Oct 20th): https://github.com/satijalab/seurat/issues/184
+# --> need to be on dev branch to fix this error.
 
 
 ##########################################################################
@@ -266,7 +324,7 @@ for (tmp.res in res2calculate) {
 seurat_merge.align <- SetAllIdent(seurat_merge.align, id=paste0("res.", res.primary)) # *IMPORTANT* set ident to the primary ident. This is needed for correct use of cluster marker gene identification
 
 # ====== SAVE IMAGE ======
-save.image(file=sprintf("%s-rsession_complete.RData", analysis_prefix))
+save.image(file=sprintf("%s-rsession_complete.RData", output_prefix))
 # ========================
 
 ##########################################################################
@@ -282,23 +340,27 @@ print("Finding markers")
 #Identify cluster markers
 cl <- makeCluster(N_CORES, type = "FORK")
 clusterEvalQ(cl, library(Seurat))
+clusterEvalQ(cl, library(tidyverse))
+clusterEvalQ(cl, library(rlang)) # needed for using UQ()
 list_of_dfs.all_markers <- parLapply(cl, cluster_ids, function(x) FindMarkers(object = seurat_merge.align, ident.1 = x, min.pct = 0.25, only.pos = TRUE, thresh.use = 0.25))
 ##df.cluster_markers <- FindAllMarkers(seurat_merge.align, only.pos=TRUE, min.pct=0.25,  thresh.use=0.25) # TAKES A LONG TIME/PARALLELIZED CODE ABOVE
 stopCluster(cl)
 list_of_dfs.all_markers <- lapply(list_of_dfs.all_markers, function(x) cbind(x,gene=rownames(x))) # add gene name as column
 names(list_of_dfs.all_markers) <- cluster_ids # set names so bind_rows will get the .id correct
-df.cluster_markers <- bind_rows(list_of_dfs.all_markers,.id="cluster_no") # combine list of dfs into a single data frame
+df.cluster_markers <- bind_rows(list_of_dfs.all_markers,.id="cluster") # combine list of dfs into a single data frame
 
-file.out.tmp <- sprintf("%s-all_markers-res_%s.csv", analysis_prefix, res.primary)
+file.out.tmp <- sprintf("%s-all_markers-res_%s.csv", output_prefix, res.primary)
 write.csv(df.cluster_markers, file=file.out.tmp, row.names=F, quote=F)
 
 
-save.image(file=sprintf("%s-rsession_complete.RData", analysis_prefix))
+save.image(file=sprintf("%s-rsession_complete.RData", output_prefix))
 
 
 # Prepare script for parallelization
 cl <- makeCluster(N_CORES, type = "FORK")
 clusterEvalQ(cl, library(Seurat))
+clusterEvalQ(cl, library(tidyverse))
+clusterEvalQ(cl, library(rlang)) # needed for using UQ()
 
 #Identify Conserved Markers that are present in BOTH conditions (grouping.var="condition")
 # we find makers for each cluster: 1 vs rest [ident.2 is NULL (default), then compare to all other cells]
@@ -306,13 +368,13 @@ list_of_dfs.conserved_markers <-parLapply(cl, cluster_ids, function(x) FindConse
 stopCluster(cl)
 list_of_dfs.conserved_markers <- lapply(list_of_dfs.conserved_markers, function(x) cbind(x,gene=rownames(x))) # add gene name as column
 names(list_of_dfs.conserved_markers) <- cluster_ids # set names so bind_rows will get the .id correct
-df.conserved_markers <- bind_rows(list_of_dfs.conserved_markers,.id="cluster_no") # combine list of dfs into a single data frame
+df.conserved_markers <- bind_rows(list_of_dfs.conserved_markers,.id="cluster") # combine list of dfs into a single data frame
 
-file.out.tmp <- sprintf("%s-conserved_markers-res_%s.csv", analysis_prefix, res.primary)
+file.out.tmp <- sprintf("%s-conserved_markers-res_%s.csv", output_prefix, res.primary)
 write.csv(df.conserved_markers, file=file.out.tmp ,row.names=F, quote=F)
 
 # ====== SAVE IMAGE ======
-save.image(file=sprintf("%s-rsession_complete.RData", analysis_prefix))
+save.image(file=sprintf("%s-rsession_complete.RData", output_prefix))
 # ========================
 
 ##########################################################################
@@ -342,13 +404,15 @@ print("Condition DE genes within cluster")
 # Identify DE genes within clusters
 cl <- makeCluster(N_CORES, type = "FORK")
 clusterEvalQ(cl, library(Seurat))
+clusterEvalQ(cl, library(tidyverse))
+clusterEvalQ(cl, library(rlang)) # needed for using UQ()
 # list_of_dfs.condition_de_genes_per_cluster <- lapply(list_of_seurat_objs_per_cluster, condition_de_genes) # NON PARALLEL VERSION. returns a named list of data frames. Names are the names from the input list.
 list_of_dfs.condition_de_genes_per_cluster <-parLapply(cl, list_of_seurat_objs_per_cluster, condition_de_genes) # PARALLEL VERSION. returns a list of data frames (not named - only 'indexes')
 stopCluster(cl)
-names(list_of_dfs.condition_de_genes_per_cluster) <- cluster_ids # NOT IMPORANT (although lapply will have wrong 'offset' for cluster_no, i.e. 1, 2, 3 instead of 0, 1, 2, ..): adding names before calling bind_rows. In this way we ensure that the cluster information is kept. this works even if condition_de_genes returns one or more "NULL" values.
-df.condition_de_genes_per_cluster <- bind_rows(list_of_dfs.condition_de_genes_per_cluster, .id="cluster_no") # returns results for all genes (no p_val_adj filter)
+names(list_of_dfs.condition_de_genes_per_cluster) <- cluster_ids # NOT IMPORANT (although lapply will have wrong 'offset' for cluster, i.e. 1, 2, 3 instead of 0, 1, 2, ..): adding names before calling bind_rows. In this way we ensure that the cluster information is kept. this works even if condition_de_genes returns one or more "NULL" values.
+df.condition_de_genes_per_cluster <- bind_rows(list_of_dfs.condition_de_genes_per_cluster, .id="cluster") # returns results for all genes (no p_val_adj filter)
 
-file.out.tmp <- sprintf("%s-condition_de_genes_per_cluster-res_%s.csv", analysis_prefix, res.primary)
+file.out.tmp <- sprintf("%s-condition_de_genes_per_cluster-res_%s.csv", output_prefix, res.primary)
 write.csv(df.condition_de_genes_per_cluster, file=file.out.tmp ,row.names=F, quote=F)
 
 
@@ -359,7 +423,7 @@ write.csv(df.condition_de_genes_per_cluster, file=file.out.tmp ,row.names=F, quo
 print("Saving session")
 
 # ====== SAVE IMAGE ======
-save.image(file=sprintf("%s-rsession_complete.RData", analysis_prefix))
+save.image(file=sprintf("%s-rsession_complete.RData", output_prefix))
 # ========================
 
 
@@ -435,19 +499,21 @@ print("Condition DE genes within cluster")
 
 # # Identify DE genes within clusters | NON-PARALLEL
 # list_of_dfs.condition_de_genes_per_cluster.pre_align <- lapply(list_of_seurat_objs_per_cluster.pre_align, condition_de_genes) # returns a named list of data frames. Names are the names from the input list.
-# df.condition_de_genes_per_cluster.pre_align <- bind_rows(list_of_dfs.condition_de_genes_per_cluster.pre_align, .id="cluster_no")
-# df.condition_de_genes_per_cluster.pre_align$cluster_no <- as.numeric(df.condition_de_genes_per_cluster.pre_align$cluster_no) - 1 # CORRECTING FOR OFF-SET (Seurat cluster IDs are zero-based. ONLY RUN THIS LINE ONCE
+# df.condition_de_genes_per_cluster.pre_align <- bind_rows(list_of_dfs.condition_de_genes_per_cluster.pre_align, .id="cluster")
+# df.condition_de_genes_per_cluster.pre_align$cluster <- as.numeric(df.condition_de_genes_per_cluster.pre_align$cluster) - 1 # CORRECTING FOR OFF-SET (Seurat cluster IDs are zero-based. ONLY RUN THIS LINE ONCE
 
 # Identify DE genes within clusters
 cl <- makeCluster(N_CORES, type = "FORK")
 clusterEvalQ(cl, library(Seurat))
+clusterEvalQ(cl, library(tidyverse))
+clusterEvalQ(cl, library(rlang)) # needed for using UQ()
 list_of_dfs.condition_de_genes_per_cluster.pre_align <-parLapply(cl, list_of_seurat_objs_per_cluster.pre_align, condition_de_genes) # PARALLEL VERSION. returns a list of data frames (not named - only 'indexes')
 stopCluster(cl)
-names(list_of_dfs.condition_de_genes_per_cluster.pre_align) <- cluster_ids.pre_align # NOT IMPORANT (although lapply will have wrong 'offset' for cluster_no, i.e. 1, 2, 3 instead of 0, 1, 2, ..): adding names before calling bind_rows. In this way we ensure that the cluster information is kept. this works even if condition_de_genes returns one or more "NULL" values.
-df.condition_de_genes_per_cluster.pre_align <- bind_rows(list_of_dfs.condition_de_genes_per_cluster.pre_align, .id="cluster_no") # returns results for all genes (no p_val_adj filter)
+names(list_of_dfs.condition_de_genes_per_cluster.pre_align) <- cluster_ids.pre_align # NOT IMPORANT (although lapply will have wrong 'offset' for cluster, i.e. 1, 2, 3 instead of 0, 1, 2, ..): adding names before calling bind_rows. In this way we ensure that the cluster information is kept. this works even if condition_de_genes returns one or more "NULL" values.
+df.condition_de_genes_per_cluster.pre_align <- bind_rows(list_of_dfs.condition_de_genes_per_cluster.pre_align, .id="cluster") # returns results for all genes (no p_val_adj filter)
 
 
-file.out.tmp <- sprintf("%s-condition_de_genes_per_cluster_pre_align-res_%s.csv", analysis_prefix, res.primary)
+file.out.tmp <- sprintf("%s-condition_de_genes_per_cluster_pre_align-res_%s.csv", output_prefix, res.primary)
 write.csv(df.condition_de_genes_per_cluster.pre_align, file=file.out.tmp ,row.names=F, quote=F)
 
 ##########################################################################
@@ -456,7 +522,7 @@ write.csv(df.condition_de_genes_per_cluster.pre_align, file=file.out.tmp ,row.na
 print("Saving session - WITH PREALIGNMENT")
 
 # ====== SAVE IMAGE ======
-save.image(file=sprintf("%s-rsession_complete.RData", analysis_prefix))
+save.image(file=sprintf("%s-rsession_complete.RData", output_prefix))
 # ========================
 
 
